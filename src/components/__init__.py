7ae4cb47cce500d45c43d03b3573df317689852d
almaz <almazgimaev@gmail.com>
2025-07-23 18:45:53 +0200

    Add new components for automation tasks, definitions, and task logs

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -6,3 +6,6 @@ from .base_deploy import BaseDeployNode
 from .base_train import BaseTrainNode
 from .redeploy_settings import RedeploySettingsNode
 from .custom_model import DeployCustomModel
+from .automation_tasks import AutomationTasksNode
+from .task_logs import TaskLogsNode
+from .definitions import DefinitionsNode

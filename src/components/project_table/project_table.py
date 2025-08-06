9da80bcfc83834c62218ce13afeb72afce318b8f
vorozhkog <vorozhkog@icloud.com>
2025-08-06 02:25:08 +0100

    add dataset table, add gui code, refactor project table

--- src/components/project_table/project_table.py
+++ src/components/project_table/project_table.py
@@ -1,19 +1,160 @@
 from datetime import datetime
-from typing import Any, Callable, Dict, List, Literal, Optional
+from typing import Any, Dict, List, Literal, Optional
 
-import pandas as pd
-
-from src.components.team_workspace_select.team_workspace_select import (
-    TeamWorkspaceSelect,
-)
-from supervisely import logger
-from supervisely._utils import abs_url
-from supervisely.api.api import Api
+from supervisely import env
+from supervisely.api.api import SERVER_ADDRESS, Api
+from supervisely.api.module_api import ApiField
 from supervisely.api.project_api import ProjectInfo
-from supervisely.app.widgets.selectable_fast_table.selectable_fast_table import (
-    SelectableFastTable,
-)
+from supervisely.app.content import DataJson, StateJson
+from supervisely.app.widgets import Container, Empty, Select, Text
+from supervisely.app.widgets.selectable_fast_table import SelectableFastTable
+from supervisely.app.widgets.widget import Widget
 from supervisely.project import ProjectType
+
+
+class TeamWorkspaceSelect(Widget):
+    class Routes:
+        VALUE_CHANGED = "value_changed"
+
+    def __init__(
+        self,
+        show_label: bool = True,
+        direction: Literal["horizontal", "vertical"] = "vertical",
+        default_team_id: Optional[int] = None,
+        default_workspace_id: Optional[int] = None,
+        size: Literal["large", "small", "mini"] = None,
+        widget_id=None,
+    ):
+        self._api = Api()
+        self._direction = direction
+        self._show_label = show_label
+        self._size = size
+        self._team_id = default_team_id
+        self._workspace_id = default_workspace_id
+        if self._team_id is None and self._workspace_id is None:
+            self._team_id = env.team_id()
+            self._workspace_id = self._api.workspace.get_list(self._team_id)[0].id
+        elif self._team_id is None and self._workspace_id is not None:
+            self._team_id = self._api.workspace.get_info_by_id(self._workspace_id).team_id
+        elif self._team_id is not None and self._workspace_id is not None:
+            actual_workspace_team_id = self._api.workspace.get_info_by_id(
+                self._workspace_id
+            ).team_id
+            if self._team_id != actual_workspace_team_id:
+                raise ValueError(
+                    f"Provided team_id {self._team_id} does not match the team of the workspace {self._workspace_id}."
+                )
+        self.team_selector.set_value(self._team_id)
+        self.workspace_selector.set_value(self._workspace_id)
+        self._style = (
+            "justify-content: center; align-items: center;" if direction == "horizontal" else ""
+        )
+        self._gap = 15 if direction == "horizontal" else 10
+        self._changes_handled = False
+        super().__init__(widget_id=widget_id, file_path=__file__)
+
+    @property
+    def _content(self):
+        return Container(
+            [
+                self.team_label,
+                self.team_selector,
+                self.workspace_label,
+                self.workspace_selector,
+            ],
+            direction=self._direction,
+            style=self._style,
+            gap=self._gap,
+        )
+
+    @property
+    def team_selector(self):
+        if not hasattr(self, "_team_selector"):
+            items = [Select.Item(team.id, team.name) for team in self._api.team.get_list()]
+            select = Select(items=items, size=self._size)
+
+            @select.value_changed
+            def on_team_change(value: int):
+                self._team_id = value
+                self._update_workspace_selector()
+
+                if hasattr(self, "_value_change_callback") and self._value_change_callback:
+                    result = {"teamId": self._team_id, "workspaceId": self._workspace_id}
+                    self._value_change_callback(result)
+
+            self._team_selector = select
+        return self._team_selector
+
+    @property
+    def workspace_selector(self):
+        if not hasattr(self, "_workspace_selector"):
+            items = [
+                Select.Item(ws.id, ws.name) for ws in self._api.workspace.get_list(self._team_id)
+            ]
+            select = Select(items=items, size=self._size)
+
+            @select.value_changed
+            def on_workspace_change(value: int):
+                self._workspace_id = value
+                StateJson()[self.widget_id]["workspaceId"] = value
+                StateJson().send_changes()
+
+                if hasattr(self, "_workspace_change_callback") and self._workspace_change_callback:
+                    result = {"teamId": self._team_id, "workspaceId": value}
+                    self._workspace_change_callback(result)
+
+            self._workspace_selector = select
+        return self._workspace_selector
+
+    @property
+    def team_label(self):
+        if not hasattr(self, "_team_label"):
+            if self._show_label:
+                self._team_label = Text(text="Team")
+            else:
+                self._team_label = Empty()
+        return self._team_label
+
+    @property
+    def workspace_label(self):
+        if not hasattr(self, "_workspace_label"):
+            if self._show_label:
+                self._workspace_label = Text(text="Workspace")
+            else:
+                self._workspace_label = Empty()
+        return self._workspace_label
+
+    def get_selected_team_id(self) -> Optional[int]:
+        return StateJson()[self.widget_id]["teamId"]
+
+    def get_selected_workspace_id(self) -> Optional[int]:
+        return StateJson()[self.widget_id]["workspaceId"]
+
+    def _update_workspace_selector(self):
+        items = [Select.Item(ws.id, ws.name) for ws in self._api.workspace.get_list(self._team_id)]
+        self.workspace_selector.set(items)
+        if self._workspace_id not in [item for item in items]:
+            self.workspace_selector.set_value(items[0].value)
+        else:
+            self.workspace_selector.set_value(self._workspace_id)
+
+    def get_json_data(self):
+        return {}
+
+    def get_json_state(self):
+        return {"teamId": self._team_id, "workspaceId": self._workspace_id}
+
+    def get_value(self):
+        """Returns the current workspace selection info"""
+        return {"teamId": self._team_id, "workspaceId": self._workspace_id}
+
+    def value_changed(self, func):
+        """Register a callback function to be called when team or workspace selection changes"""
+        self._value_change_callback = func
+        self._changes_handled = True
+        self._workspace_change_callback = func
+
+        return func
 
 
 class ProjectTable(SelectableFastTable):
@@ -33,23 +174,15 @@ class ProjectTable(SelectableFastTable):
         self._api = Api()
         self._team_id = team_id
         self._workspace_id = workspace_id
-        self._allowed_project_types = allowed_project_types
 
-        if isinstance(self._allowed_project_types, list):
-            if all(isinstance(pt, ProjectType) for pt in self._allowed_project_types):
-                self._allowed_project_types = [pt.value for pt in self._allowed_project_types]
-
-        self._projects = []
-        self._project_id_to_info = {}
-
-        columns = ["Project Name", "ID", "Date Modified", "Type", "Assets"]
+        columns = ["Project", "ID", "Date Modified", "Type", "Assets"]
 
         sort_by_to_column_idx = {
             "name": 0,
             "date": 2,
             "assets": 4,
         }
-        sort_column_idx = sort_by_to_column_idx.get(sort_by)
+        sort_column_idx = sort_by_to_column_idx.get(sort_by, 2)
 
         columns_options = [
             {"customCell": True},  # Project Name column
@@ -59,22 +192,40 @@ class ProjectTable(SelectableFastTable):
             {},  # Assets column
         ]
 
+        @self.team_workspace_select.value_changed
+        def on_team_workspace_change(value: Dict[str, int]):
+            self._team_id, self._workspace_id = value.values()
+            self.loading = True
+            self._refresh_table()
+            self.loading = False
+
+        if self._team_id is None:
+            self._team_id = self.team_workspace_select.get_selected_team_id()
+        if self._workspace_id is None:
+            self._workspace_id = self.team_workspace_select.get_selected_workspace_id()
+
+        self._projects = []
+        self._project_id_to_info = {}
+        self._allowed_project_types = allowed_project_types
+        if isinstance(self._allowed_project_types, list):
+            if all(isinstance(pt, ProjectType) for pt in self._allowed_project_types):
+                self._allowed_project_types = [pt.value for pt in self._allowed_project_types]
+
+        table_data = self._get_table_data()
         super().__init__(
-            data=[],
+            data=table_data,
             columns=columns,
             columns_options=columns_options,
             page_size=page_size,
             sort_column_idx=sort_column_idx,
             sort_order=sort_order,
             width=width,
-            is_selectable=True,
-            max_selected_rows=1,
-            header_left_content=self.team_workspace_select,
             widget_id=widget_id,
+            show_header=True,
+            is_selectable=True,
+            header_left_content=self.team_workspace_select,
+            max_selected_rows=1,
         )
-
-        if self._workspace_id:
-            self._refresh_table()
 
     @property
     def team_workspace_select(self) -> TeamWorkspaceSelect:
@@ -85,42 +236,63 @@ class ProjectTable(SelectableFastTable):
                 default_team_id=self._team_id,
                 default_workspace_id=self._workspace_id,
             )
-
-            @selector.value_changed
-            def on_team_workspace_change(value: Dict[str, int]):
-                self._team_id = selector.get_selected_team_id()
-                self._workspace_id = selector.get_selected_workspace_id()
-                self._refresh_table()
-
             self._team_workspace_select = selector
         return self._team_workspace_select
 
-    def _refresh_table(self):
+    def _get_table_data(self) -> List[List[Any]]:
         """Refresh the table with current workspace projects."""
-        projects = self._api.project.get_list(self._workspace_id)
+        if not self._workspace_id:
+            return []
+        projects = self._api.project.get_list(
+            self._workspace_id,
+            fields=[
+                ApiField.ID,
+                ApiField.NAME,
+                ApiField.TYPE,
+                ApiField.CREATED_AT,
+                ApiField.IMAGES_COUNT,
+                ApiField.REFERENCE_IMAGE_URL,
+            ],
+        )
         if self._allowed_project_types:
             projects = [p for p in projects if p.type in self._allowed_project_types]
+        id_to_preview = {
+            info.id: info.image_preview_url
+            for info in projects
+            if info.image_preview_url.rstrip("/") != SERVER_ADDRESS
+        }
 
         self._projects = projects
         self._project_id_to_info = {p.id: p for p in projects}
 
         table_data = []
-        for i, project in enumerate(projects):
+        for project in projects:
             row_data = [
                 (
                     project.name,
-                    abs_url(project.reference_image_url),
-                ),  # Tuple for image injection
+                    id_to_preview.get(project.id, None),
+                ),
                 project.id,
                 datetime.strptime(
                     project.created_at.replace("Z", ""), "%Y-%m-%dT%H:%M:%S.%f"
                 ).strftime("%d %b %Y %H:%M"),
                 project.type.replace("_", " ").title(),
-                project.items_count,
+                project.items_count or 0,
             ]
             table_data.append(row_data)
 
-        self.read_pandas(pd.DataFrame(table_data, columns=self._columns_first_idx))
+        return table_data
+
+    def _refresh_table(self):
+        """Refresh the table with current workspace projects."""
+        table_data = self._get_table_data()
+        self._source_data = self._prepare_input_data(table_data)
+        (
+            self._parsed_source_data,
+            self._sliced_data,
+            self._parsed_active_data,
+        ) = self._prepare_working_data()
+        self._filter_changed()
 
     def get_selected_project(self) -> Optional[ProjectInfo]:
         """Returns the selected project info.
@@ -174,3 +346,78 @@ class ProjectTable(SelectableFastTable):
     #         func(selected_project)
 
     #     return _on_selection_changed
+
+
+# @ TODO: Autoselect all datasets on init
+class DatasetTable(SelectableFastTable):
+    def __init__(
+        self,
+        project_id: int,
+        sort_by: Literal["id", "name", "created_at", "items"] = "id",
+        sort_order: Literal["asc", "desc"] = "asc",
+        page_size: int = 10,
+        width: str = "auto",
+        widget_id: Optional[str] = None,
+    ):
+        self._api = Api()
+        self._project_id = project_id
+
+        columns = ["Dataset", "ID", "Created At", "Items"]
+
+        sort_by_to_column_idx = {
+            "name": 0,
+            "id": 1,
+            "created_at": 2,
+            "items": 3,
+        }
+        sort_column_idx = sort_by_to_column_idx.get(sort_by, 1)
+
+        columns_options = [
+            {"customCell": True},  # Dataset Name column
+            {},  # ID column
+            {},  # Created at column
+            {},  # Items column
+        ]
+
+        table_data = self._get_table_data()
+        super().__init__(
+            data=table_data,
+            columns=columns,
+            columns_options=columns_options,
+            page_size=page_size,
+            sort_column_idx=sort_column_idx,
+            sort_order=sort_order,
+            width=width,
+            widget_id=widget_id,
+            show_header=True,
+            is_selectable=True,
+            header_left_content=None,
+        )
+
+    def _get_table_data(self) -> List[List[Any]]:
+        """Refresh the table with current project datasets."""
+        datasets = []
+        id_to_image_preview = {}
+        id_to_full_name = {}
+        for parents, dataset_info in self._api.dataset.tree(self._project_id):
+            full_name = "/".join(parents + [dataset_info.name])
+            id_to_full_name[dataset_info.id] = full_name
+            id_to_image_preview[dataset_info.id] = dataset_info.image_preview_url
+            datasets.append(dataset_info)
+
+        table_data = []
+        for dataset in datasets:
+            row_data = [
+                (
+                    id_to_full_name.get(dataset.id, dataset.name),
+                    id_to_image_preview.get(dataset.id, None),
+                ),
+                dataset.id,
+                datetime.strptime(
+                    dataset.created_at.replace("Z", ""), "%Y-%m-%dT%H:%M:%S.%f"
+                ).strftime("%d %b %Y %H:%M"),
+                dataset.items_count or 0,
+            ]
+            table_data.append(row_data)
+
+        return table_data
--- src/components/training_data.py
+++ src/components/training_data.py
@@ -1,14 +1,187 @@
 from typing import List, Literal, Optional
 
+from src.components.project_table.project_table import DatasetTable, ProjectTable
 from supervisely.api.api import Api
 from supervisely.app import DataJson
-from supervisely.app.widgets import Dialog, FastTable, Icons, SolutionCard, Widget
-from supervisely.project import ProjectType
+from supervisely.app.widgets import (
+    Button,
+    Container,
+    Dialog,
+    Empty,
+    Flexbox,
+    Icons,
+    ReloadableArea,
+    SolutionCard,
+    Text,
+    Widget,
+)
+from supervisely.project.project_type import ProjectType
 from supervisely.solution.base_node import SolutionCardNode, SolutionElement
 
 
 class TrainingDataGUI(Widget):
-    pass
+    class ActivePage:
+        PROJECT = "project"
+        DATASET = "dataset"
+        SPLITS = "splits"
+
+    def __init__(
+        self,
+        api: Api,
+        team_id: Optional[int] = None,
+        workspace_id: Optional[int] = None,
+    ):
+        self.api = api
+        self.team_id = team_id
+        self.workspace_id = workspace_id
+
+        super().__init__(file_path=__file__)
+
+    @property
+    def modal(self) -> Dialog:
+        if not hasattr(self, "_modal"):
+            desc = (
+                "Skip the initial steps of data import and annotation "
+                "by copying already labeled data from your existing projects to "
+                "Training Project. Select a team, workspace, and project — and the "
+                "app will automatically detect and split datasets using naming "
+                "conventions (train/val) or your defined Train/Val Split settings. "
+                "All selected data will be copied into your training project "
+                "and organized into collections for immediate use."
+            )
+            self._modal = Dialog(
+                title="Copy Training Data from existing projects",
+                content=Container(
+                    [
+                        Text(desc),
+                        self.reloadable_area,
+                        Flexbox(
+                            [
+                                Empty(),
+                                Empty(),
+                                Empty(),
+                                self.back_btn,
+                                self.next_btn,
+                                self.run_btn,
+                            ]
+                        ),
+                    ]
+                ),
+            )
+            self._set_modal_by_active_page()
+        return self._modal
+
+    @property
+    def active_page(self) -> Literal["project", "dataset", "splits"]:
+        return DataJson()[self.widget_id].get("active_page", "project")
+
+    @active_page.setter
+    def active_page(self, value: Literal["project", "dataset", "splits"]):
+        if value not in self.ActivePage.__dict__.values():
+            raise ValueError("active_page must be either 'project', 'dataset' or 'splits'")
+        DataJson()[self.widget_id]["active_page"] = value
+        DataJson().send_changes()
+
+    @property
+    def reloadable_area(self) -> ReloadableArea:
+        if not hasattr(self, "_reloadable_area"):
+            self._reloadable_area = ReloadableArea()
+        return self._reloadable_area
+
+    @property
+    def next_btn(self) -> Button:
+        if not hasattr(self, "_next_btn"):
+            self._next_btn = Button(
+                "Next",
+                icon="zmdi zmdi-arrow-right",
+                style="primary",
+            )
+
+            @self._next_btn.click
+            def _on_next_click():
+                if self.active_page == self.ActivePage.PROJECT:
+                    self.active_page = self.ActivePage.DATASET
+                elif self.active_page == self.ActivePage.DATASET:
+                    self.active_page = self.ActivePage.SPLITS
+                elif self.active_page == self.ActivePage.SPLITS:
+                    self.active_page = self.ActivePage.PROJECT
+                self._set_modal_by_active_page()
+
+        return self._next_btn
+
+    @property
+    def back_btn(self) -> Button:
+        if not hasattr(self, "_back_btn"):
+            self._back_btn = Button(
+                "Back",
+                icon="zmdi zmdi-arrow-left",
+                style="primary",
+            )
+
+            @self._back_btn.click
+            def _on_back_click():
+                if self.active_page == self.ActivePage.DATASET:
+                    self.active_page = self.ActivePage.PROJECT
+                elif self.active_page == self.ActivePage.SPLITS:
+                    self.active_page = self.ActivePage.DATASET
+                self._set_modal_by_active_page()
+
+        return self._back_btn
+
+    @property
+    def run_btn(self) -> Button:
+        if not hasattr(self, "_run_btn"):
+            self._run_btn = Button(
+                "Run",
+                icon="zmdi zmdi-play",
+                style="primary",
+            )
+        return self._run_btn
+
+    @property
+    def project_table(self) -> ProjectTable:
+        if not hasattr(self, "_project_table"):
+            self._project_table = ProjectTable(
+                team_id=self.team_id,
+                workspace_id=self.workspace_id,
+                allowed_project_types=[ProjectType.IMAGES],
+            )
+        return self._project_table
+
+    @property
+    def dataset_table(self) -> DatasetTable:
+        if not hasattr(self, "_dataset_table"):
+            self._dataset_table = DatasetTable(
+                project_id=self.project_table.get_selected_project().id,
+            )
+        return self._dataset_table
+
+    @property
+    def splits_table(self) -> Container:
+        if not hasattr(self, "_splits_table"):
+            self._splits_table = Container(
+                [
+                    Text("Not implemented yet"),
+                ]
+            )
+        return self._splits_table
+
+    def _set_modal_by_active_page(self) -> Widget:
+        if self.active_page == self.ActivePage.PROJECT:
+            self.reloadable_area.set_content(self.project_table)
+            self.back_btn.hide()
+            self.next_btn.show()
+            self.run_btn.hide()
+        elif self.active_page == self.ActivePage.DATASET:
+            self.reloadable_area.set_content(self.dataset_table)
+            self.back_btn.show()
+            self.next_btn.show()
+            self.run_btn.hide()
+        elif self.active_page == self.ActivePage.SPLITS:
+            self.reloadable_area.set_content(self.splits_table)
+            self.back_btn.show()
+            self.next_btn.hide()
+            self.run_btn.show()
 
 
 class TrainingDataNode(SolutionElement):
@@ -16,40 +189,39 @@ class TrainingDataNode(SolutionElement):
         self,
         api: Api,
         title: str,
-        description: str,
         width: int = 250,
         x: int = 0,
         y: int = 0,
         team_id: Optional[int] = None,
         workspace_id: Optional[int] = None,
         icon: Optional[Icons] = None,
-        tooltip_position: Literal["left", "right"] = "right",
         *args,
         **kwargs,
     ):
         self.api = api
         self.title = title
-        self.description = description
         self.width = width
         self.icon = icon or Icons(
             "zmdi zmdi-collection-folder-image",
             color="#1976D2",
             bg_color="#E3F2FD",
         )
-        self.tooltip_position = tooltip_position
         super().__init__(*args, **kwargs)
 
-        self.card = self._create_card()
+        self.main_widget = TrainingDataGUI(api=self.api, team_id=team_id, workspace_id=workspace_id)
+
+        @self.card.click
+        def _on_click():
+            self.main_widget.modal.show()
+
+        @self.main_widget.run_btn.click
+        def _on_run_click():
+            self.main_widget.modal.hide()
+            self.run()
+
+        self.card = SolutionCard(title=self.title, icon=self.icon)
         self.node = SolutionCardNode(content=self.card, x=x, y=y)
-        self.modals = []
+        self.modals = [self.main_widget.modal]
 
-    def _create_card(self) -> SolutionCard:
-        return SolutionCard(
-            title=self.title,
-            tooltip=self._create_tooltip(),
-            icon=self.icon,
-            tooltip_position=self.tooltip_position,
-        )
-
-    def _create_tooltip(self) -> SolutionCard.Tooltip:
-        return SolutionCard.Tooltip(description=self.description)
+    def run(self):
+        pass
